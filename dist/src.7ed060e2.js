74c8b276c93f192f0ac61609ce0db8dc80af5dc8
Qixuan Hong <frederickhong.5@gmail.com>
2019-04-15 12:27:48 -0500

    add babel to project

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -4272,7 +4272,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43219" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40353" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
